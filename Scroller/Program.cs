911c0c98f760ab68981e4ca40e2c6adfdc673f17
spor <sporp@ssemi.cz>
2024-06-20 20:51:56 +0200

    new

--- Scroller/Program.cs
+++ Scroller/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using System.Threading;
 
 namespace Scroller
 {
@@ -13,31 +14,58 @@ namespace Scroller
     {
         static void Main(string[] args)
         {
-            var driver = new ChromeDriver();
-            Console.WriteLine("---------------------");
-            Console.WriteLine("input URL: ");
+            Console.Write("input URL: ");
             string url = Console.ReadLine();
+            Console.Write("input scroll speed (pixels per milisecond): ");
+            string speed = Console.ReadLine();
 
-            driver.Url = "https://www.nuget.org/packages/Selenium.WebDriver";
+            //https://www.w3.org/TR/webdriver/#keyboard-actions
 
+            var driver = new ChromeDriver();
+            
+            driver.Url = url;
+            string prevUrl = driver.Url;
+
+            Final(driver, speed);
+            while (true)
+            {
+                while (driver.Url != prevUrl)
+                {
+                    prevUrl = driver.Url;
+                    Final(driver, speed);
+                }
+            }
+            
+        }
+
+        public static void ScrollDown(WebDriver driver, int height, string speed)
+        {
+            string exeString = "window.scrollBy(0,)";
+            string modExeString = exeString.Insert(18, speed);
+            int intSpeed = Int32.Parse(speed);
+
+            for (int i = 0; i < height/intSpeed; i++)
+            {
+                ((IJavaScriptExecutor)driver).ExecuteScript(modExeString, "");
+            }
+        }
+
+        public static void SnapTop(WebDriver driver, int height)
+        {
+            new Actions(driver)
+                .ScrollByAmount(0, -height + 1000)
+                .Perform();
+        }
+
+        public static void Final(WebDriver driver, string speed)
+        {
             var h = driver.ExecuteScript("return document.body.scrollHeight");
             int height = int.Parse(h.ToString());
 
-            scrollDown(driver, height);
-            snapTop(driver, height);
-        }
-        public static void scrollDown(WebDriver driver, int height)
-        {
-            for (int i = 0; i < height; i++)
-            {
-                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollBy(0,1)", "");
-            }
-        }
-        public static void snapTop(WebDriver driver, int height)
-        {
-            new Actions(driver)
-                .ScrollByAmount(0, -height)
-                .Perform();
+            SnapTop(driver, height);
+
+            Thread.Sleep(1000);
+            ScrollDown(driver, height, speed);
         }
     }
 }
